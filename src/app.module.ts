f5b65108e77dafe8263eef434637f52b12a153d7
Caio Bozato Aroca <bozato@ibm.com>
2020-08-18 22:53:47 -0300

    feat(Auth): implemented middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { AuthMiddleware } from './middlewares/auth.middleware';
 
 @Module({
   imports: [ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(AuthMiddleware).forRoutes('products');
+  }
+}
