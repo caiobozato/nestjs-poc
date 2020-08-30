de74419a26ce6f64ace35e586e73d0ca6a45ec43
Caio Bozato Aroca <bozato@ibm.com>
2020-08-30 20:14:37 -0300

    docs(Swagger): mapped all routes in swagger

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -8,53 +8,101 @@ import {
   Delete,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import {
+  ApiOperation,
+  ApiHeader,
+  ApiTags,
+  ApiCreatedResponse,
+  ApiUnauthorizedResponse,
+  ApiBody,
+  ApiOkResponse,
+} from '@nestjs/swagger';
+
 import { Product } from './product.model';
 
+@ApiHeader({
+  name: 'auth',
+  description: 'Authentication header',
+})
+@ApiTags('Products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  async addProduct(
-    @Body('title') prodTitle: string,
-    @Body('description') prodDescription: string,
-    @Body('price') prodPrice: number,
-  ): Promise<{ id: string }> {
-    const productId = await this.productsService.insertProduct(
-      prodTitle,
-      prodDescription,
-      prodPrice,
-    );
+  @ApiOperation({ summary: 'Creates new product' })
+  @ApiBody({
+    description: 'New product data',
+    required: true,
+    type: Product,
+  })
+  @ApiCreatedResponse({
+    description: 'Returns id for created product',
+    schema: { example: { id: '5f47afdd39178d2a543f45e4' } },
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  async addProduct(@Body() product: Product): Promise<{ id: string }> {
+    const productId = await this.productsService.insertProduct(product);
     return { id: productId };
   }
 
   @Get()
+  @ApiOperation({ summary: 'Gets all products' })
+  @ApiOkResponse({
+    description: 'Returns an array of products',
+    type: Product,
+    isArray: true,
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   async getProducts(): Promise<any[]> {
     const products = await this.productsService.getProducts();
     return products;
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Find a product by id' })
+  @ApiOkResponse({ description: 'Returns a product', type: Product })
+  @ApiUnauthorizedResponse({ description: 'Not authorized' })
   getProductById(@Param('id') id: string): any {
     return this.productsService.getProductById(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a single product' })
+  @ApiOkResponse({
+    schema: {
+      example: {
+        id: '5f47afdd39178d2a543f45e4',
+        message: 'Product updated successfully',
+      },
+    },
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   async updateProduct(
     @Param('id') id: string,
     @Body('title') title: string,
     @Body('description') description: string,
     @Body('price') price: number,
-  ): Promise<null> {
+  ): Promise<{ id: string; message: string }> {
     await this.productsService.updateProduct(id, title, description, price);
-    return null;
+    return { id, message: 'Product updated successfully' };
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a single product' })
+  @ApiOkResponse({
+    schema: {
+      example: {
+        id: '5f47afdd39178d2a543f45e4',
+        message: 'Product deleted successfully',
+      },
+    },
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   async deleteProductById(
     @Param('id') id: string,
-  ): Promise<{ message: string }> {
+  ): Promise<{ id: string; message: string }> {
     await this.productsService.deleteProduct(id);
-    return { message: `Product with id ${id}, deleted` };
+    return { id, message: 'Product deleted successfully' };
   }
 }
